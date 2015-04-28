f0cc0c2b734461ff24ef9a3d946b8daecb7efc1c
Alexey Kolosov <alexey.kolosov@gmail.com>
2015-04-28 08:57:31 +0500

    Some fixes

--- startup.sh
+++ startup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 echo "=> Starting and running RethinkDB..."
 
-/usr/bin/rethinkdb --directory "/data/db" --log-file "/data/logs/rethinkdb.log" @$
+/usr/bin/rethinkdb --directory /data/db --log-file /data/logs/rethinkdb.log @$
